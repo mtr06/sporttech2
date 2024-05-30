247fc2ebbfd2e0cbccc31d3aea6fccf5711feec7
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2024-05-30 23:25:09 +0700

    Update paymentconfirmation.tsx

--- app/(tabs)/paymentconfirmation.tsx
+++ app/(tabs)/paymentconfirmation.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { db } from '@/firebaseConfig';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 
 const PaymentConfirmation = () => {
   const router = useRouter();
+  const params = useLocalSearchParams();
+  const id = params.id as string;
+  const selectedTimes = params.selectedTimes ? params.selectedTimes.split(',') : [];
+  console.log(id);
+  console.log(selectedTimes);
+
+  const convertHourToNumber = (hourString) => {
+    return parseInt(hourString.split(':')[0]);
+  };
+
+  const updateFirestoreData = async (selectedTimes) => {
+    try {
+      const venueRef = doc(db, 'lapangan', id);
+
+      for (const selectedTime of selectedTimes) {
+        if (!selectedTime.includes('_')) {
+          console.error(`Invalid format for selected time: ${selectedTime}`);
+          continue;
+        }
+
+        const [day, hour] = selectedTime.split('_');
+        const hourNumber = convertHourToNumber(hour);
+
+        // Constructing the path for the document to update
+        const dayRef = `timeAvailable.${day}.${hourNumber}`;
+
+        // Updating the document
+        await updateDoc(venueRef, {
+          [dayRef]: false,
+        });
+
+        console.log(`Updated ${day} ${hour} to false`);
+      }
+
+      console.log('Firestore data updated successfully');
+    } catch (error) {
+      console.error('Error updating Firestore data:', error);
+    }
+  };
+
+  useEffect(() => {
+    updateFirestoreData(selectedTimes);
+  }, []);
 
   return (
     <View style={styles.container}>
